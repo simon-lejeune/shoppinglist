d774b5cd09b7b31cac18c1aabb54cb35e3c9ae25
Simon Lejeune <lejeunesimon.91@gmail.com>
2022-06-23 16:12:56 +0100

    [ADD] github workflows: linters
    also run black on the all the files of the project

--- backend/shoppinglist/core/migrations/0001_initial.py
+++ backend/shoppinglist/core/migrations/0001_initial.py
@@ -8,42 +8,61 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Item',
+            name="Item",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('content', models.CharField(max_length=200)),
-                ('is_processed', models.BooleanField(default=False)),
-                ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('updated_at', models.DateTimeField(auto_now=True)),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("content", models.CharField(max_length=200)),
+                ("is_processed", models.BooleanField(default=False)),
+                ("created_at", models.DateTimeField(auto_now_add=True)),
+                ("updated_at", models.DateTimeField(auto_now=True)),
             ],
             options={
-                'ordering': ['-created_at'],
+                "ordering": ["-created_at"],
             },
         ),
         migrations.CreateModel(
-            name='List',
+            name="List",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=200)),
-                ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('updated_at', models.DateTimeField(auto_now=True)),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("name", models.CharField(max_length=200)),
+                ("created_at", models.DateTimeField(auto_now_add=True)),
+                ("updated_at", models.DateTimeField(auto_now=True)),
             ],
             options={
-                'ordering': ['-created_at'],
+                "ordering": ["-created_at"],
             },
         ),
         migrations.AddConstraint(
-            model_name='list',
-            constraint=models.CheckConstraint(check=models.Q(('name__length__gt', 0)), name='name_length'),
+            model_name="list",
+            constraint=models.CheckConstraint(
+                check=models.Q(("name__length__gt", 0)), name="name_length"
+            ),
         ),
         migrations.AddField(
-            model_name='item',
-            name='list_id',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='core.list'),
+            model_name="item",
+            name="list_id",
+            field=models.ForeignKey(
+                on_delete=django.db.models.deletion.CASCADE, to="core.list"
+            ),
         ),
     ]
