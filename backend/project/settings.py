9dd22de1be353aa3e9061d8427517a9718e03f03
Simon Lejeune <lejeunesimon.91@gmail.com>
2022-06-28 09:38:53 +0100

    [REF] backend: get DEBUG and SECRET_KEY settings from environment

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -11,19 +11,22 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env(
+    DEBUG=(bool, False),
+)
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-uc(57i0mqby-egi&ns_(rj(uh-smif4^jaum#a06&ly5xbq4_s"
+SECRET_KEY = env("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
