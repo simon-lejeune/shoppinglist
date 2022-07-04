2c7f9c79f6e16c4460ae4378abd8eb8055b79ad7
Simon Lejeune <lejeunesimon.91@gmail.com>
2022-07-04 16:36:45 +0100

    [REF] backend: expose id field with the api on both list and item

--- backend/shoppinglist/core/serializers.py
+++ backend/shoppinglist/core/serializers.py
@@ -5,10 +5,10 @@ from rest_framework import serializers
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
-        fields = ["list_id", "content", "is_processed", "created_at", "updated_at"]
+        fields = ["id", "list_id", "content", "is_processed", "created_at", "updated_at"]
 
 
 class ListSerializer(serializers.ModelSerializer):
     class Meta:
         model = List
-        fields = ["name", "created_at", "updated_at"]
+        fields = ["id", "name", "created_at", "updated_at"]
