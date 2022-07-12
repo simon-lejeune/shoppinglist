9997276cc469951b3c66d204694fb0b221954d04
Simon Lejeune <lejeunesimon.91@gmail.com>
2022-07-12 16:30:13 +0100

    [ADD] mobile: theme

--- mobile/src/navigation/index.js
+++ mobile/src/navigation/index.js
@@ -1,14 +1,23 @@
 import { Items } from '@app/screens/items';
 import { Lists } from '@app/screens/lists';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useTheme } from 'react-native-paper';
 
 const Stack = createNativeStackNavigator();
 
 export const ShoppingListStack = () => {
+  const { colors } = useTheme();
+  const headerStyle = {
+    backgroundColor: colors.primary,
+  };
+  const headerTitleStyle = {
+    color: '#e9ecf5',
+  };
+
   return (
     <Stack.Navigator initialRouteName="Lists">
-      <Stack.Screen name="Lists" component={Lists} />
-      <Stack.Screen name="Items" component={Items} />
+      <Stack.Screen name="Lists" component={Lists} options={{ headerStyle, headerTitleStyle }} />
+      <Stack.Screen name="Items" component={Items} options={{ headerStyle, headerTitleStyle }} />
     </Stack.Navigator>
   );
 };
