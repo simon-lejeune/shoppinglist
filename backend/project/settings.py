09524c44ffac4709cd5fe304fccca5467d7f92fe
Simon Lejeune <lejeunesimon.91@gmail.com>
2022-07-14 12:55:41 +0100

    [REF] backend: allowed hosts settings

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env("SECRET_KEY")
 
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = ["shoppinglist.slap.agency"]
+ALLOWED_HOSTS = ["shoppinglist.slap.agency", "api.shoppinglist.slap.agency"]
 if DEBUG:
     ALLOWED_HOSTS = ["*"]
 
